0744978b575f55cd7200960c9b7e21824ac9152c
Juan Perdomo - Jakepy <perdomojuan187@gmail.com>
2022-08-29 17:03:52 -0500

    02 Tipos de datos

---
+++ 02-tiposDeDatos/tipos.js
@@ -0,0 +1,57 @@
+// Comentarios de una sola linea
+
+/*
+Comentarios de multiples lineas
+Hola yo soy jalkepys
+*/
+
+//! Tipos de datos
+
+//* Numero - number
+console.log(29);
+console.log(3.1415);
+
+//* Texto - Strings
+console.log("Jakepys Perdomo");
+console.log("Jakepys Perdomo");
+console.log(`Jakepys Santos`);
+
+//* Logicos - Boolean
+console.log(true);
+console.log(false);
+
+//* indefinido - undefined
+console.log(undefined);
+
+//* null (Ausencia de valor)
+console.log(null);
+
+/*
+! ********************************************* 
+! Tipos de datos estructural
+*/
+{
+  property: "value";
+}
+// key       value
+// ejemplo de objeto
+console.log({
+  nombre: "jakepy",
+  edad: 19,
+  estudiante: true,
+  linuxero: true,
+  OsWindows: null,
+});
+
+// Array
+["values", "values2", "values3", "valuesn..."];
+// array de datos variados
+console.log([
+  true,
+  1,
+  false,
+  null,
+  [undefined, { name: "jakepy" }],
+  { cosas: "bill gates" },
+]);
+//! ***************************************
