af5ab373a57cf2b64dc3639c7b8734e9d2120c4b
Juan Perdomo - Jakepy <perdomojuan187@gmail.com>
2022-08-29 17:04:24 -0500

    04 Operadores

---
+++ 04-operadores/operadores.js
@@ -0,0 +1,14 @@
+/* 
+! Operadores
+*/
+
+// Operador de asignacion
+var x = 3;
+
+// Operador de asignacion de adicion (+=)
+var x = 2;
+var y = 1;
+
+console.log((x += y));
+
+// y haci mismo con el resto de opedadores (+, -. *, /, **, %)
